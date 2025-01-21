615968417a1c289e6f73fb90708a6052594081f7
Tidiane <114936529+Tidi95@users.noreply.github.com>
2025-01-21 13:47:37 +0100

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,25 @@
-module.exports = (req, res, next) => {
-   console.log('Middleware d\'authentification');
-   next();
-};
+const jwt = require('jsonwebtoken');
 
+// Middleware d'authentification
+module.exports = (req, res, next) => {
+   const authHeader = req.headers['authorization'];
+   
+   // Vérification de la présence du token dans l'en-tête Authorization
+   if (!authHeader) {
+      return res.status(401).json({ message: 'Accès refusé. Aucun token fourni.' });
+   }
+
+   const token = authHeader.split(' ')[1]; // Récupérer la partie après "Bearer"
+   
+   if (!token) {
+      return res.status(401).json({ message: 'Token invalide ou manquant.' });
+   }
+
+   try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Vérification du token avec la clé secrète
+      req.user = decoded; // Attacher les données de l'utilisateur décodées à l'objet requête
+      next(); // Passer à la route suivante
+   } catch (error) {
+      return res.status(403).json({ message: 'Token invalide ou expiré.' });
+   }
+};
